33c28145f5b7db019e37fa0a6cb0bdf0d86c9aff
Makhabane <makhabaneramakoro05@gmail.com>
2025-04-30 04:21:46 +0200

    Describe the changes you made

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors({
 }));
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
